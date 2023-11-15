d1cff346d0bf10ff95cfbc881a70955df80d0121
Xyves <xodrixx@protonmail.com>
2023-11-15 17:42:20 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -17,14 +16,10 @@ const config = {
     open: true,
     host: "localhost",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./dist/index.html",
-    }),
 
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  // Add your plugins here
+  // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+
   module: {
     rules: [
       {
@@ -34,6 +29,10 @@ const config = {
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
 
       // Add your rules for custom modules here
