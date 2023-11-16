08629e5eb1356a1acae49439904ad2c18be8de82
Xyves <xodrixx@protonmail.com>
2023-11-16 17:01:28 +0100

    Change directory of imported files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import inbox from "./inbox";
-import resetContent from "./resetPage";
+import inbox from "./Modules/inbox";
+import resetContent from "./Modules/resetPage";
+import addMenuEvents from "./menuEvents";
 // import nextWeek from "./NextWeek";
 // import today from "./today";
 // import Completed from "./Completed";
 document.addEventListener("DOMContentLoaded", function () {
   resetContent();
   inbox();
+  addMenuEvents();
 });
