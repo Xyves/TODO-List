dab1c196c44f045adb53b4ce5fdb1b3952f10445
Xyves <xodrixx@protonmail.com>
2023-12-05 12:00:25 +0100

    Remove local storage module

--- src/Modules/localStorage.js
+++
@@ -1,6 +0,0 @@
-// Not yet made
-export default function (object) {
-  const task = JSON.parse(window.localStorage.getItem(object));
-  localStorage.setItem("Data", JSON.stringify(object));
-  console.log(localStorage);
-}
