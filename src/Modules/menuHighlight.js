80b6cffa9b14b7ca3f40c2d045ccf336bc8b3ea3
Xyves <xodrixx@protonmail.com>
2023-12-11 22:38:55 +0100

    Remove setTimeout

--- src/Modules/menuHighlight.js
+++ src/Modules/menuHighlight.js
@@ -1,29 +1,10 @@
 // Add class to a project element onclick
 export default function HighlightSidebar() {
-  const content = document.querySelector(".content");
   const allDefaultMenuElements = document.querySelectorAll(".menuElement");
 
   allDefaultMenuElements.forEach((menuElement) => {
     menuElement.addEventListener("click", function () {
-      setTimeout(() => {
-        const mainHeader = document.querySelector(".content h1");
-
-        menuElement.classList.add("sidebar-active");
-        content.prepend(mainHeader);
-      }, 0);
+      menuElement.classList.add("sidebar-active");
     });
   });
-}
-export function getSelectedPage() {
-  const selectedElement = document.querySelector(".sidebar-active");
-  if (selectedElement) {
-    console.log(selectedElement.textContent.trim());
-    return selectedElement.textContent.trim();
-  }
-}
-export function highlightAllProjects() {
-  const elements = document.querySelectorAll(".menuEl");
-  for (let i = 0; i < elements.length; i++) {
-    HighlightSidebar();
-  }
 }
