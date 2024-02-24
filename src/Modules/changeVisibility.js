94b6456c0c036e57f0347c2fecd92b548d844f7b
Xyves <xodrixx@protonmail.com>
2024-02-24 22:13:44 +0100

    add HideSidebar function

--- src/Modules/changeVisibility.js
+++ src/Modules/changeVisibility.js
@@ -151,3 +151,10 @@ function removeOption(text) {
     optionToRemove.remove();
   }
 }
+function hideSidebar(){
+  const button = document.querySelector(".fa-square-caret-left")
+  const sidebar = document.querySelector(".sidebar")
+  button.addEventListener("click",()=>{
+sidebar.classList.toggle("sidebar-show")
+  })
+}
